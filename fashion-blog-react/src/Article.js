6ce80b8577ce227aa588ffca5ffa59e44ce05895
Quincy-Bee <quincybee@icloud.com>
2026-07-18 22:00:37 -0400

    Article js

---
+++ fashion-blog-react/src/Article.js
@@ -0,0 +1,27 @@
+function Article({date, title, image}) {
+
+    return (
+        <article>
+
+            <p>{date}</p>
+
+            <h2>{title}</h2>
+
+            <img src={image} alt={title}/>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Velit ducimus aut amet eum sunt nemo autem eligendi
+                aspernatur deserunt facilis in dolor quia quo eos
+                voluptatum labore.
+            </p>
+
+            <a className="continue" href="#">
+                continues...
+            </a>
+
+        </article>
+    );
+}
+
+export default Article;
