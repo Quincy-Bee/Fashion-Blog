c4b409b824c8ef2e30edc21e41cc3f11ea30f3dc
Quincy-Bee <quincybee@icloud.com>
2026-07-18 22:10:32 -0400

    react testing

--- fashion-blog-react/src/Nav.js
+++ fashion-blog-react/src/Nav.js
@@ -1,16 +1,16 @@
 function Nav() {
-    return (
-        <nav>
-            <ul className="nav">
-                <li><a href="#">Women's</a></li>
-                <li><a href="#">Men's</a></li>
-                <li><a href="#">On the Street</a></li>
-                <li><a href="#">The Catwalk</a></li>
-                <li><a href="#">AdWatch</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className="nav">
+        <li><a href="#">Women's</a></li>
+        <li><a href="#">Men's</a></li>
+        <li><a href="#">On the Street</a></li>
+        <li><a href="#">The Catwalk</a></li>
+        <li><a href="#">AdWatch</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
