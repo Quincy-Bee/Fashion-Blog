0161747cde2e234d21de46696f161e0782e65784
Quincy-Bee <quincybee@icloud.com>
2026-07-18 21:59:49 -0400

    header js

---
+++ fashion-blog-react/src/Header.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+
+function Header() {
+    return (
+        <header>
+
+            <h1>Sartre's List</h1>
+
+            <p>Better-Dressed People</p>
+
+            <Nav />
+
+        </header>
+    );
+}
+
+export default Header;
